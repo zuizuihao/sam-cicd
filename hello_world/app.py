7f6f0a46bd29bb810d1504b94b3a80da1f1b24ac
EC2 Default User <ec2-user@ip-10-0-0-74.us-west-2.compute.internal>
2019-07-11 14:30:57 +0000

    update app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world! v1.2",
+            "message": "hello world! v1.3",
             "location": ip.text.replace("\n", "")
         }),
     }
